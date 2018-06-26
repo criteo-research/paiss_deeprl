1630365828c0cae1247fdd5febb5d9ec45fc6d68
Kiewan VILLATEL <k.villatel@criteo.com>
2018-06-26 14:20:17 +0200

    Move gamma in RLDebugger

--- utils.py
+++ utils.py
@@ -19,7 +19,6 @@ class RLEnvironment(object):
         self.observation_space = self.env.observation_space
         self.target_perf = target_perf
         self.target_window = target_window
-        self.gamma = 0.995
 
     def run(self, agent, episodes=100, print_delay=10, display_policy=False, seed=None):
         """
@@ -126,6 +125,7 @@ class RLDebugger:
 
     def __init__(self):
         self.traces = Trace()
+        self.gamma = 0.995
 
     def record(self, action, state, target, target_val, loss, reward):
         self.traces += ('action', action)
